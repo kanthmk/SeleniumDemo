acfaa7fb41466af162b91e56fa18f020e9397ea4
Mahesh kanth <maheshkanthb@gmail.com>
2021-06-25 04:15:17 +0530

    Comment added in testcase

--- SeleniumPOM/src/test/java/com/testcases/LoginPageTest.java
+++ SeleniumPOM/src/test/java/com/testcases/LoginPageTest.java
@@ -1,11 +1,12 @@
+/*
+ * author MaheshKanth
+ * 
+*/
 package com.testcases;
-
-import java.util.concurrent.TimeUnit;
 
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
 import com.base.BaseClass;
 import com.pages.HomePage;
 import com.pages.LoginPage;
